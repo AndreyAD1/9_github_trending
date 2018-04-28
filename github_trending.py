dd700ea79e29b64760252bcfbd4cc85ca2c7f92e
Андрей Аракельянц <arakelyants.andrey@yandex.ru>
2018-04-28 10:35:56 +0300

    Fix some code details.

--- github_trending.py
+++ github_trending.py
@@ -9,38 +9,38 @@ def get_first_date_of_request(days_number):
     return first_date_of_request
 
 
-def get_sorted_repos_created_after_a_date(filter_date):
+def get_sorted_repos_created_after_a_date(first_date_of_request):
     github_repos_url = 'https://api.github.com/search/repositories'
-    creation_date_argument = 'created:>={}'.format(filter_date)
+    creation_date_argument = 'created:>={}'.format(first_date_of_request)
     request_arguments = {'q': creation_date_argument, 'sort': 'stars'}
     try:
-        all_repos_request = requests.get(
+        repository_response = requests.get(
             github_repos_url,
             params=request_arguments
         )
-        return all_repos_request.json()
+        return repository_response.json()
     except requests.exceptions.RequestException:
         return None
 
 
 def get_most_popular_repositories(all_repositories, output_repos_number):
-    all_repos_number = all_repositories['total_count']
-    if all_repos_number < output_repos_number:
+    received_repos_number = all_repositories['total_count']
+    if received_repos_number < output_repos_number:
         output_repos_number = output_repos_number
     most_popular_repositories = all_repositories['items'][:output_repos_number]
     return most_popular_repositories
 
 
-def print_repositories(repo_list):
+def print_repository_info(repository_list):
     print('Most popular repositories created this week:')
-    for repository in repo_list:
-        repo_url = repository['html_url']
-        repo_stars = repository['stargazers_count']
-        repo_issues = repository['open_issues']
-        print('URL: {}, stars: {}, open issues: {}'.format(
-            repo_url,
-            repo_stars,
-            repo_issues
+    for repository in repository_list:
+        repository_url = repository['html_url']
+        repository_stars = repository['stargazers_count']
+        repository_issues = repository['open_issues']
+        print('{}, stars: {}, open issues: {}'.format(
+            repository_url,
+            repository_stars,
+            repository_issues
         ))
 
 
@@ -56,4 +56,4 @@ if __name__ == '__main__':
     trending_repositories = get_most_popular_repositories(
         all_repos_info, max_number_of_repos
     )
-    print_repositories(trending_repositories)
+    print_repository_info(trending_repositories)
